55e216dd6c08a9d60712f337b170ced67e8124d3
bastioncillo <33004217+bastioncillo@users.noreply.github.com>
2018-05-18 13:06:23 +0200

    harpoon in progrees

--- src/superPang.js
+++ src/superPang.js
@@ -21,26 +21,17 @@ var game = function() {
 		stage.insert(new Q.Up_Edge());
 		stage.insert(new Q.Down_Edge());
 		stage.insert(new Q.Player());
-		stage.insert(new Q.Bolas5());
 	});
 
 
 
 //-------------LOAD_RESOURCES----------------
-	Q.load(["backgrounds.png", "backgrounds.json", "player.png", "player.json", 
-			"left_edge.png", "right_edge.png", "down_edge.png", "up_edge.png",
-			"bolas1.png", "bolas1.json", "bolas2.png", "bolas2.json", "bolas3.png", "bolas3.json",
-			"bolas4.png", "bolas4.json", "bolas5.png", "bolas5.json", "bolasEspeciales.png", "bolasEspeciales.json"],
-
+	Q.load(["backgrounds.png", "backgrounds.json", "left_edge.png",
+			"right_edge.png", "player.png", "player.json",
+			"down_edge.png", "up_edge.png", "harpoon.png"],
 			function() {
 		Q.compileSheets("backgrounds.png", "backgrounds.json");
 		Q.compileSheets("player.png", "player.json");
-		Q.compileSheets("bolas1.png", "bolas1.json");
-		Q.compileSheets("bolas2.png", "bolas2.json");
-		Q.compileSheets("bolas3.png", "bolas3.json");
-		Q.compileSheets("bolas4.png", "bolas4.json");
-		Q.compileSheets("bolas5.png", "bolas5.json");
-		Q.compileSheets("bolasEspeciales.png", "bolasEspeciales.json");
 
 		Q.stageScene("back", 0);
 		Q.stageScene("level", 1);
@@ -49,16 +40,12 @@ var game = function() {
 
 //-------------ANIMATIONS-------------
 	Q.animations("player", {
-		move_right: 	{ frames: [3,0,1,2],  rate: 1/8, flip:"right" }, 
-		move_left: 		{ frames: [3,0,1,2],  rate: 1/8, flip:"left" },
-		stand: 			{ frames: [4],		  rate: 1/9 },
+		move_left: 		{ frames: [3,0,1,2],  rate: 1/8, flip:"left" }, 
+		move_right: 	{ frames: [3,0,1,2],  rate: 1/8, flip:"right" },
+		stand: 			{ frames: [4],		  rate: 1/8 },
 		died: 			{ frames: [6],		  rate: 1,	 loop: false, trigger:"dying" },
 		victory:  		{ frames: [7],		  rate: 1,	 loop: false },
-		shoot:  		{ frames: [5],		  rate: 1/9, loop: false, trigger:"shooted" }
-	});
-
-	Q.animations("bolasEspeciales", {
-		change: {frames: [0,1,2,3], rate: 3}
+		shoot:  		{ frames: [5],		  rate: 1/8, loop: false, trigger:"shooted" ,next: "stand"}
 	});
 
 //-------------BACKGROUND_SPRITE----------------
@@ -117,7 +104,6 @@ var game = function() {
 		}
 	});
 
-
 //-------------UP_EDGE_SPRITE----------------
 	Q.Sprite.extend("Up_Edge",{
 		init:function(p){
@@ -132,166 +118,13 @@ var game = function() {
 		}
 	});
 
-//-------------BOLAS1_SPRITE----------------
-	Q.Sprite.extend("Bolas1",{
-		init:function(p){
-			this._super(p,{
-				sprite: "bolas1",
-				sheet: "bolas1",
-				frame:0,
-				x:200,
-				y:200,
-				vx: 175,
-				gravity: 0.5,
-				scale:2
-			});
-			this.add('2d, aiBounce');
-		},
-
-		step: function() {
-			this.on("bump.bottom, bump.top", function(collision) {
-				if (collision.obj.isA("Down_Edge")) {
-					this.p.vy = -700;
-				}
-
-				if (collision.obj.isA("Up_Edge")) {
-					this.p.vy = 350;
-				}
-			})						
-		}
-	});
-
-//-------------BOLAS2_SPRITE----------------
-	Q.Sprite.extend("Bolas2",{
-		init:function(p){
-			this._super(p,{
-				sprite: "bolas2",
-				sheet: "bolas2",
-				frame:0,
-				x:200,
-				y:200,
-				vx: 175,
-				gravity: 0.5,
-				scale:2
-			});
-			this.add('2d, aiBounce');
-		},
-
-		step: function() {
-			this.on("bump.bottom", function(collision) {
-				if (collision.obj.isA("Down_Edge")) {
-					this.p.vy = -625;
-				}
-			})						
-		}
-	});
-
-//-------------BOLAS3_SPRITE----------------
-	Q.Sprite.extend("Bolas3",{
-		init:function(p){
-			this._super(p,{
-				sprite: "bolas3",
-				sheet: "bolas3",
-				frame:0,
-				x: 200,
-				y: 200,
-				vx: 175,
-				gravity: 0.5,
-				scale:2
-			});
-			this.add('2d, aiBounce');
-		},
-
-		step: function() {
-			this.on("bump.bottom", function(collision) {
-				if (collision.obj.isA("Down_Edge")) {
-					this.p.vy = -550;
-				}
-			})						
-		}
-	});
-
-//-------------BOLAS4_SPRITE----------------
-	Q.Sprite.extend("Bolas4",{
-		init:function(p){
-			this._super(p,{
-				sprite: "bolas4",
-				sheet: "bolas4",
-				frame:0,
-				x:200,
-				y:200,
-				vx: 175,
-				gravity: 0.5,
-				scale:2
-			});
-			this.add('2d, aiBounce');
-		},
-
-		step: function() {
-			this.on("bump.bottom", function(collision) {
-				if (collision.obj.isA("Down_Edge")) {
-					this.p.vy = -450;
-				}
-			})						
-		}
-	});
-
-//-------------BOLAS5_SPRITE----------------
-	Q.Sprite.extend("Bolas5",{
-		init:function(p){
-			this._super(p,{
-				sprite: "bolas5",
-				sheet: "bolas5",
-				frame:0,
-				x:200,
-				y:200,
-				vx: 175,
-				gravity: 0.5,
-				scale:2
-			});
-			this.add('2d, aiBounce');
-		},
-
-		step: function() {
-			this.on("bump.bottom", function(collision) {
-				if (collision.obj.isA("Down_Edge")) {
-					this.p.vy = -350;
-				}
-			})						
-		}
-	});
-
-//-------------BOLASESPECIALES_SPRITE----------------
-	Q.Sprite.extend("BolasEspeciales",{
-		init:function(p){
-			this._super(p,{
-				sprite: "bolasEspeciales",
-				sheet: "bolasEspeciales",
-				vx: 175,
-				x:200,
-				y:200,
-				gravity: 0.5,
-				scale:2
-			});
-			this.add('2d, animation, aiBounce');
-		}, 
-
-		step:function(){
-			this.play("change", 1);
-
-			this.on("bump.bottom", function(collision) {
-				if (collision.obj.isA("Down_Edge")) {
-					this.p.vy = -550;
-				}
-			})
-		}
-	});		
-
 //-------------HARPOON_SPRITE----------------
 	Q.Sprite.extend("Harpoon",{
 		init:function(p){
 			this._super(p,{
-				asset:"harpoon.png"
+				asset:"harpoon.png",
+				scale:2,
+				y:282
 			});
 		}
 	});
@@ -304,14 +137,15 @@ var game = function() {
 				sheet:"player",
 				frame:4,
 				x:400,
-				y:520,
+				y:400,
 				playing: true,
-				scale:2
+				scale:2,
+				jumpSpeed:0
 			});
 			this.add('2d, platformerControls, animation');
 			
 			/*The user push the fire button*/
-			Q.input.on("fire");
+			Q.input.on("fire", this,"fire");
 
 			/* Wait until the firing animation has played until
 			 actually launching the harpoon*/
@@ -320,28 +154,27 @@ var game = function() {
 
 		step:function(dt){
 			if(this.p.playing){
-				if(this.p.vx > 0) {
-					this.p.vx = 350;
+				if(this.p.vx > 0){
+					this.p.vx = 400;
 					this.play("move_right", 1);
 				}
-        	
-				else if (this.p.vx < 0) {
-					this.p.vx = -350;
+				else if(this.p.vx < 0){
+					this.p.vx = -400;
 					this.play("move_left", 1);
 				}
-
 				else
 					this.play("stand", 1);
 			}
 		},
 
-		shoot:function() {
-			this.play("shoot");
+		fire:function(){
+			this.play("shoot", 3);
 		},
 
-		launchHarpoon: function(stage){
+		launchHarpoon: function(){
 			var harpoon = new Q.Harpoon({x:this.p.x});
-			stage.insert(harpoon);
+			this.stage.insert(harpoon);
 		}
+
 	});
 }
